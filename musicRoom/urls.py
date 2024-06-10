500941d902e8a08923923fa42c07504134423d8a
RickSanchez88E <k21078271@kcl.ac.uk>
2024-06-10 16:28:17 +0100

    add basic dummy template

--- musicRoom/urls.py
+++ musicRoom/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+import views
+    
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', views.index, name='index'),
+    path('', views.home, name='home')
 ]
