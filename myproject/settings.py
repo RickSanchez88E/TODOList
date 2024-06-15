aea6ba44d3d155140c5fe6604c8e2bbaea7c6faa
Rick Sanchez <110410215+RickSanchez88E@users.noreply.github.com>
2024-06-15 12:37:51 +0100

    update settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2@yst2ov4nj2e2!n@pa1gurh%%fcz+(=44az6!uzir$2vha8$9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["www.macroking.top"]
 
 
 # Application definition
